b830403dc1bb96be5cef4a311884355bc9868894
romeromatias64 <matiasaromero03@gmail.com>
2024-12-27 20:46:42 -0300

    sas

---
+++ js/movie-apps.js
@@ -0,0 +1,26 @@
+const movies = []
+
+// Obtener el formulario de carga de peliculas desde el DOM
+const moviesForm = document.getElementById("moviesForm")
+
+// Necesito escuchar cuando el usuario envie el formulario
+// En base a lo ingresado por el usuario, crear un objeto de pelicula
+
+moviesForm.addEventListener("submit", function() {
+    console.log("El formulario fue enviado")
+})
+
+// Agregar la pelicula al array de peliculas
+
+
+
+
+
+
+// pelicula = {
+//     title: "The Godfather",
+//     genre: "Drama",
+//     year: "1972",
+//     score: 5,
+//     url: 'https://www.imdb.com/title/tt0068646/'
+// }
